51601d5a989f7b494d951755dc898fb86519334f
rdalin82 <rdalin@benchprep.com>
2017-10-07 08:39:47 -0500

    descending order at index

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -1,9 +1,21 @@
 module Api::V1
 	class IdeasController < ApplicationController
 		def index
-			@ideas = Idea.all
+			@ideas = Idea.order('created_at DESC')
 			render json: @ideas
 		end
+
+    def create
+      @idea = Idea.create(idea_params)
+      render json: @data
+    end
+
+    private
+
+    def idea_params
+      params.require(:idea).permit(:title, :body)
+    end
+
 	end
 end
 
